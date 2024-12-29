4f457a12b20e987c55c7680b47f89f4a19a92443
peter-nv <nvhg2803@gmail.com>
2024-12-29 22:05:35 +0700

    update

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/nvhoang2803",
+      GitHub: "https://github.com/peter-nv",
     },
   }),
 }
